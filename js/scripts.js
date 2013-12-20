8a4d9597c7b825c5cefadf36e16e0ccde4b81934
24kyle64 <24kyle64@gmail.com>
2013-12-20 14:32:06 -0800

    base

---
+++ js/scripts.js
@@ -0,0 +1,85 @@
+// JavaScript Document
+
+$(document).ready(function(){
+	 
+	 $('.curtains').curtain({
+           scrollSpeed: 300,
+           controls: '.menu',
+           curtainLinks: '.curtain-links',
+           nextSlide: function(){
+            console.log("ok");
+           }
+       });
+	   
+	    $.backstretch("img/wood.jpg");
+
+	 $('.image-link').magnificPopup({type:'image'});
+	
+
+	$("#bose").capslide({
+                    caption_color	: 'white',
+                    caption_bgcolor	: 'black',
+                    overlay_bgcolor : 'black',
+                    border			: '',
+                    showcaption	    : false
+                });
+				
+				$("#3d").capslide({
+                    caption_color	: 'white',
+                    caption_bgcolor	: 'black',
+                    overlay_bgcolor : 'black',
+                    border			: '',
+                    showcaption	    : false
+                });
+				$("#music").capslide({
+                    caption_color	: 'white',
+                    caption_bgcolor	: 'black',
+                    overlay_bgcolor : 'black',
+                    border			: '',
+                    showcaption	    : false
+                });
+				$("#don").capslide({
+                    caption_color	: 'white',
+                    caption_bgcolor	: 'black',
+                    overlay_bgcolor : 'black',
+                    border			: '',
+                    showcaption	    : false
+                });
+				$("#spreads").capslide({
+                    caption_color	: 'white',
+                    caption_bgcolor	: 'black',
+                    overlay_bgcolor : 'black',
+                    border			: '',
+                    showcaption	    : false
+                });
+				$("#care").capslide({
+                    caption_color	: 'white',
+                    caption_bgcolor	: 'black',
+                    overlay_bgcolor : 'black',
+                    border			: '',
+                    showcaption	    : false
+                });
+				$("#undefeated").capslide({
+                    caption_color	: 'white',
+                    caption_bgcolor	: 'black',
+                    overlay_bgcolor : 'black',
+                    border			: '',
+                    showcaption	    : false
+                });
+				$("#logo").capslide({
+                    caption_color	: 'white',
+                    caption_bgcolor	: 'black',
+                    overlay_bgcolor : 'black',
+                    border			: '',
+                    showcaption	    : false
+                });
+				$("#stuff").capslide({
+                    caption_color	: 'white',
+                    caption_bgcolor	: 'black',
+                    overlay_bgcolor : 'black',
+                    border			: '',
+                    showcaption	    : false
+                });
+	
+	
+});
